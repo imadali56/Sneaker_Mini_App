d67611bf1db1fc3adc1c5362a86349e24dbb8136
imadali56 <gcpian7038@gmail.com>
2026-01-10 23:50:56 +0500

    intro screen

---
+++ android/app/src/main/kotlin/com/sneakerShop/sneaker_shop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sneakerShop.sneaker_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:sneaker_shop/screens/home_screen.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(backgroundColor: Colors.grey[300]),
+      body: Column(
+        children: [
+
+          Center(
+
+            child: Image.asset('assets/images/nike_logo.png', height: 100),
+          ),
+          SizedBox(height: 100),
+          // Title
+          Text('Just Do It', style: TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 20,
+          ),),
+          // Sub-Title
+          Text(
+            'Exclusive new sneakers and custom kicks made with top-tier premium quality', textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.grey, fontSize: 16),
+          ),
+          SizedBox(height: 20),
+          // Shop Now Button
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            // container
+            child: GestureDetector(
+              onTap: (){
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+    },
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                // shop now text
+                child: Center(
+                  child: Text('Shop Now', style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.white, fontSize: 16,
+                  ),),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
