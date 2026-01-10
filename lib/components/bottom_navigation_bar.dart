cb2bf2978e93056d9db8cb4b9b559ce035c8667f
imadali56 <gcpian7038@gmail.com>
2026-01-11 00:45:10 +0500

    Add Bottom Navigation Bar to Home Screen

---
+++ lib/components/bottom_navigation_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  const MyBottomNavBar({super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: GNav(
+        color: Colors.grey.shade400,
+        activeColor: Colors.grey.shade700,
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabBackgroundColor: Colors.grey.shade100,
+        tabBorderRadius: 17,
+        mainAxisAlignment: MainAxisAlignment.center,
+        tabs: [
+          GButton(icon: Icons.home, text: 'Shop'),
+          GButton(icon: Icons.shopping_bag_rounded, text: 'Cart'),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,21 @@
 
 import 'package:flutter/material.dart';
-class HomeScreen extends StatelessWidget {
+
+import '../components/bottom_navigation_bar.dart';
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home Screen'),),
+      backgroundColor: Colors.grey[300],
+     bottomNavigationBar: MyBottomNavBar(),
     );
+
   }
 }
