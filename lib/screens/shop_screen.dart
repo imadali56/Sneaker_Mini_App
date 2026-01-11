82514014426f373a8a81e7a1b51576331b31c20a
imadali56 <gcpian7038@gmail.com>
2026-01-12 02:30:40 +0500

    Search bar and update in shop screen

--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -11,6 +11,59 @@ class ShopScreen extends StatefulWidget {
 class _ShopScreenState extends State<ShopScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('shop screen', style: TextStyle(color: Colors.pink),));
+    return Column(
+      children: [
+        // Search Bar
+        Container(
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+borderRadius: BorderRadius.circular(10)
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Search', style: TextStyle(
+                color: Colors.grey
+              ),),
+              Icon(Icons.search,  color: Colors.grey),
+            ],
+          ),
+        ),// search bar
+        // Message
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25),
+          child: Text('Everyone starts in sneakers.. Never stop', style: TextStyle(
+            color: Colors.grey[600]
+          ),),
+        ),
+        // hot picks and see all
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              // // hot Picks
+              Text('Hot Picks 🔥', style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold
+              ),),
+              // see all
+              Text('See all',style: TextStyle(
+                color: Colors.blue, fontWeight: FontWeight.bold
+              ),)
+            ],
+          ),
+        ),
+        SizedBox(height: 10),
+        Expanded(child: ListView.builder(
+            itemBuilder: (context, index){
+              return ShoeTile();
+            }
+
+        ))
+      ],
+    );
   }
 }
