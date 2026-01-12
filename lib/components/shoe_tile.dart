017347fe0ca1163e3336c8b9c5e6c44a3c83cb9d
imadali56 <gcpian7038@gmail.com>
2026-01-12 21:57:43 +0500

    Implemented a scrollable product list on the Home Screen

---
+++ lib/components/shoe_tile.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:sneaker_shop/models/shoe.dart';
+
+class ShoeTile extends StatelessWidget {
+  Shoe shoe;
+
+  ShoeTile({super.key, required this.shoe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 25),
+      width: 280,
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // shoe pic
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(shoe.imagePath),
+          ), // clip RRect
+          // description
+          Text(
+            shoe.description,
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+          // price+details
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                  children: [
+                    // shoe name
+                    Text(
+                      shoe.name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    // price
+                    Text(shoe.price, style: TextStyle(color: Colors.grey)),
+                  ],
+                ),
+
+                // plus button
+                Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12),
+                    ),
+                  ),
+
+                  child: Icon(Icons.add, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          // button to add to cart
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sneaker_shop/models/shoe.dart';
+
+import '../components/shoe_tile.dart';
 
 
 class ShopScreen extends StatefulWidget {
@@ -18,24 +21,25 @@ class _ShopScreenState extends State<ShopScreen> {
           padding: EdgeInsets.all(12),
           margin: EdgeInsets.symmetric(horizontal: 25),
           decoration: BoxDecoration(
-            color: Colors.grey[200],
-borderRadius: BorderRadius.circular(10)
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10)
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text('Search', style: TextStyle(
-                color: Colors.grey
+                  color: Colors.grey
               ),),
-              Icon(Icons.search,  color: Colors.grey),
+              Icon(Icons.search, color: Colors.grey),
             ],
           ),
-        ),// search bar
+        ), // search bar
         // Message
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 25),
-          child: Text('Everyone starts in sneakers.. Never stop', style: TextStyle(
-            color: Colors.grey[600]
+          child: Text(
+            'Everyone starts in sneakers.. Never stop', style: TextStyle(
+              color: Colors.grey[600]
           ),),
         ),
         // hot picks and see all
@@ -47,19 +51,28 @@ borderRadius: BorderRadius.circular(10)
             children: [
               // // hot Picks
               Text('Hot Picks 🔥', style: TextStyle(
-                fontSize: 24, fontWeight: FontWeight.bold
+                  fontSize: 24, fontWeight: FontWeight.bold
               ),),
               // see all
-              Text('See all',style: TextStyle(
-                color: Colors.blue, fontWeight: FontWeight.bold
+              Text('See all', style: TextStyle(
+                  color: Colors.blue, fontWeight: FontWeight.bold
               ),)
             ],
           ),
         ),
         SizedBox(height: 10),
         Expanded(child: ListView.builder(
-            itemBuilder: (context, index){
-              return ShoeTile();
+          itemCount: 4,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              // create a show
+              Shoe shoe = Shoe(name: 'Air Flex',
+                  price: '\$34',
+                  imagePath: 'assets/images/shoes_4.png',
+                  description: 'Flexible design, all-day comfort');
+              return ShoeTile(
+                shoe: shoe,
+              );
             }
 
         ))
